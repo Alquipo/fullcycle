0926f12cca8487d0f082a767aef464c8a2ee7a44
Alquipo Neto <alquiponeto@outlook.com.br>
2023-04-26 22:46:28 -0300

    fix: bugs node container

--- docker/node/index.js
+++ docker/node/index.js
@@ -2,23 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database:'nodedb'
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
 };
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Wesley')`
+const sql = `INSERT INTO people(name) values('Alquipo')`
 connection.query(sql)
 connection.end()
 
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>Full Cycle</h1>')
 })
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
+app.listen(port, () => {
+  console.log('Rodando na porta ' + port)
 })
